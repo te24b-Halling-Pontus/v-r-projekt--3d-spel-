c64435e1b3ce8e24eb0c04beebcda56fb7fa927d
Pontus hallig <pontushalling08@gmail.com>
2026-02-10 16:25:51 +0100

    lekt med 3d saker med jump och fps ochh grejor, bra commit va?

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,51 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField]
+    float walkSpeed;
+    Vector2 moveInput;
+    CharacterController controller;
+
+    [SerializeField]
+    float gravityMuilt = 2;
+    [SerializeField]
+    float jumpForce = 200;
+
+    float velocetyY = 0;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        velocetyY += Physics.gravity.y * gravityMuilt * Time.deltaTime;
+
+        if (controller.isGrounded && velocetyY < 0)
+        {
+            velocetyY = 0;
+        }
+
+        Vector3 movement = transform.forward * moveInput.y + transform.right * moveInput.x;
+
+        movement *= walkSpeed;
+
+        movement.y = velocetyY;
+
+        controller.Move(movement * Time.deltaTime);
+    }
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    void OnJump(InputValue value)
+    {
+        if (controller.isGrounded)
+        {
+            velocetyY = jumpForce;
+        }
+    }
+}
